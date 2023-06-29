90ca1a697f709b6934788e618a9d4c775c07795e
Valdemir rodrigues Goncalves Junior <72047928+ValdemirJunior2020@users.noreply.github.com>
2023-06-29 15:07:25 -0400

    Add files via upload

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,17 +1,39 @@
-import express from "express";
-import User from "../db/Models/UserSchema.js";
+import express from 'express';
+import User from '../db/Models/UserSchema.js';
 
 const UserRouter = express.Router();
+UserRouter.use(express.json());
 
-// Create a new user
-UserRouter.post("/register", async (req, res) => {
+UserRouter.get('/', async (req, res) => {
   try {
-    const { name, email, password } = req.body;
-    const user = await User.create({ name, email, password });
-    res.status(200).json({ msg: "User created successfully", user: user });
+    const users = await User.find();
+    res.status(200).json(users);
   } catch (error) {
-    res.status(400).json({ error: "Failed to create user." });
+    res.status(500).json({ error: 'Failed to retrieve users. The gremlins stole them!' });
   }
 });
 
+UserRouter.post('/register', async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.create({ name, email, password });
+    res.status(200).json({ msg: 'User registered successfully', user: user });
+  } catch (error) {
+    res.status(400).json({ msg: 'Failed to register user. Maybe the aliens hacked our system!' });
+  }
+});
+
+
+UserRouter.post("/login", async (req, res)=>{
+  try {
+        const {email, password}= req.body;
+        const existUser =await User.findOne({email, password})
+        if(!existUser){
+          res.status(400).json({error: "user not Found, have you Registered?"})
+        }
+        res.status(201).json({msg: "user logged in", user: existUser})
+  } catch (error) {
+      res.status(404).json({error: error})
+  }
+})
 export default UserRouter;
