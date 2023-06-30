15edc42038e8ae421f4649fbbc8f0d654064caa3
Valdemir rodrigues Goncalves Junior <72047928+ValdemirJunior2020@users.noreply.github.com>
2023-06-30 14:21:26 -0400

    Add files via upload

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import User from '../db/Models/UserSchema.js';
-
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 const UserRouter = express.Router();
 UserRouter.use(express.json());
 
@@ -15,15 +16,19 @@ UserRouter.get('/', async (req, res) => {
 
 UserRouter.post('/register', async (req, res) => {
   try {
-    const { name, email, password } = req.body;
-    const user = await User.create({ name, email, password });
+    const { name, email, password} = req.body;
+    if (name && email && password){
+      const hashPassword = await bcrypt.hash(password, 10)
+    const user = await User.create({ name, email, password: hashPassword});
     res.status(200).json({ msg: 'User registered successfully', user: user });
+    }else{
+      res.status(400).json({msg: "you forgot your email, name or password"})
+    }
+    
   } catch (error) {
     res.status(400).json({ msg: 'Failed to register user. Maybe the aliens hacked our system!' });
   }
 });
-
-
 UserRouter.post("/login", async (req, res)=>{
   try {
         const {email, password}= req.body;
